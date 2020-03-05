e2690837ab30a6de455b30bff0474ba9195a18f9
Gretchen <46028399+Osminog-Riz@users.noreply.github.com>
2020-03-06 00:35:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,11 +15,12 @@ print('Мяу, мир!');
   <meta charset="utf-8">
 </head> 
 <body> 
+ 
 <br><img class="img" src="11.png" alt="1"> 
 <br><img class="img" src="2.png" alt="2"> 
 <br><img class="img" src="3.png" alt="3"> 
 <br><img class="img" src="4.png" alt="4"> 
-<br><img class="img" src="5.png" alt="5"> 
+<br><img class="img" src="55.png" alt="5"> 
 <br><img class="img" src="6.png" alt="6"> 
 <br><img class="img" src="7.png" alt="7"> 
 
