4103d526ffa8050bfc086173550fbb1b65581e7c
Gretchen <46028399+Osminog-Riz@users.noreply.github.com>
2020-03-05 23:21:36 +0300

    Update index.php

--- files/index.php
+++ files/index.php
@@ -8,10 +8,17 @@ header('Content-Type: text/html; charset=UTF-8');
 // отправленный комментарий в ответе веб-сервера.
 print_r($_POST);
 
-print('Привет, мир!');
+print('Мяу, мир!');
 ?>
 <html>
   <head></head>
   <body>
+    <img class="img" src="1.png" alt="1">
+     <img class="img" src="2.png" alt="2">
+     <img class="img" src="3.png" alt="3">
+     <img class="img" src="4.png" alt="4">
+     <img class="img" src="5.png" alt="5">
+     <img class="img" src="6.png" alt="6">
+     <img class="img" src="7.png" alt="7">
     
   </body>
