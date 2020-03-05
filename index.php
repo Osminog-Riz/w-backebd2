d7d75478bbadd9cfbc11ccf5e935b79fa0b7a0ef
Gretchen <46028399+Osminog-Riz@users.noreply.github.com>
2020-03-06 00:28:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ print('Мяу, мир!');
 <html> 
 <head></head> 
 <body> 
-<img class="img" src="1.png" alt="1"> 
+<img class="img" src="11.png" alt="1"> 
 <img class="img" src="2.png" alt="2"> 
 <img class="img" src="3.png" alt="3"> 
 <img class="img" src="4.png" alt="4"> 
